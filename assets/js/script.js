e0f013aabc73ecb59663a8626925ef87691c61a8
Justin Welch <justin.welch075@gmail.com>
2021-04-19 18:00:00 -0500

    first question renders when start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 var timerEl = document.getElementById('timer');
+var questionEl = document.getElementById('question1');
+var one = document.getElementById('1');
+var two = document.getElementById('2');
+var three = document.getElementById('3');
+var four = document.getElementById('4');
 var timer = 60;
 var clearTime;
 
@@ -21,6 +26,37 @@ var startTimer = function() {
     }
 
     clearTime = endTimer;
+
+    quest1();
 }
+
+var quest1 = function () {
+    questionEl.innerHTML = 'How do you render "Hello World" in an alert box?';
+
+    one.innerHTML = '<button id="a">aletBox("Hello World");</button>';
+    two.innerHTML = '<button id="b">console.log("Hello World");</button>';
+    three.innerHTML = '<button id="c">alert("Hello World");</button>';
+    four.innerHTML = '<button id="d">prompt("Hello World");</button>';
+
+    var a = document.getElementById("a");
+    var b = document.getElementById("b");
+    var c = document.getElementById("c");
+    var d = document.getElementById("d");
+
+    var clickedAnswer = function() {
+        wrongAnswer();
+        quest2();
+    }
+
+    a.addEventListener("click", clickedAnswer);
+    b.addEventListener("click", clickedAnswer);
+    c.addEventListener("click", quest2);
+    d.addEventListener("click", clickedAnswer);
+}
+
+
+
+
+
 
 start.onclick = startTimer;
